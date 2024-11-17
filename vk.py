e11211599c18ce0fa2aaa817d00bc8af31ca3178
AldeR <koalder@yandex.ru>
2024-11-17 18:10:54 +0300

    В vk.py добавил код для добавления записей в базу данных

--- vk.py
+++ vk.py
@@ -1,22 +1,34 @@
 import requests  # Импортируем библиотеку requests для работы с HTTP-запросами
 from pprint import pprint  # Импортируем pprint для удобного форматирования вывода
 import json  # Импортируем json для работы с JSON-данными
+import sqlite3 # Импортирует sqlite3 для работы с базами данных
 
 token = ('vk1.a.yxxhoAU0X_NN86KDgr58ONhwvxX0TY4KPBM2Ut07EUKbsRlrW_db4PjxJNK2PcEMe0V2'
          'QvXgXLk0SD36rBFcBzvg0R-FQhxCnv-xn_wC93frumLrJy8_LEoZngLo0G_Bo4L_yr22z7rfQM'
          'mXm9f3X7_bvG914Ho25oHv-R3jkrq2vVHTM02I9ohTVK4lU1QbldY0LdFkgJ1MiZfY9Gnqww')  # Токен доступа к API ВКонтакте
-
 method = 'users.search'  # Метод API для поиска пользователей
 url = f'https://api.vk.com/method/{method}'  # Формируем URL для запроса к API
-
 params = {  # Параметры запроса к API
     'access_token': token,  # Токен доступа
     'q': 'Свечников',  # Запрос на поиск по имени (по моей фамилии)
     'v': '5.199',  # Версия API
-    'hometown': ['Йошкар-Ола'],  # Мой город Йошкар-Ола для фильтрации результатов
+    'hometown': 'Йошкар-Ола',  # Мой город Йошкар-Ола для фильтрации результатов
     'fields': 'education, home_town'  # Поля, которые нужно вернуть в ответе (образование и город)
 }
 
-result = requests.get(url, params=params)  # Отправляем GET-запрос к API с указанными параметрами
-result.encoding = 'UTF-8'  # Устанавливаем кодировку ответа на UTF-8 на всякий
-pprint(result.json())  # Выводим ответ в формате JSON с удобным форматированием
+response = requests.get(url, params=params)  # Отправляем GET-запрос к API с указанными параметрами
+response.encoding = 'UTF-8'  # Устанавливаем кодировку ответа на UTF-8 на всякий
+result = response.json() # Сохраняем в переменной результаты поиска в в формате JSON
+
+sql = sqlite3.connect('vk.sqlite') # Подключаемся к базе
+cursor = sql.cursor() # Создаём курсор
+
+for n, user in enumerate(result['response']['items']):
+    cursor.execute("insert or ignore into first_name (name) VALUES (?)", # Делаем SQL-запрос на внесение записи
+                   (user['first_name'],)) # Вносимые значения (имя)
+    cursor.execute("insert or ignore into university_name (id, name) VALUES (?, ?)",  # Делаем SQL-запрос на внесение записи
+                   (user['university'], user['university_name']))  # Вносимые значения (номер и название универа)
+    cursor.execute("insert or ignore into svecha_yoshka (id, first_name_id, university_id) VALUES (?, ?, ?)",
+                   # Делаем SQL-запрос на внесение записи
+                   (user['id'], n+1, user['university']))  # Вносимые значения (VK ID, id имени и универа)
+    sql.commit() # Вносим собранные изменения в базу данных
